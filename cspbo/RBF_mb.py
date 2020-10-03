now it works, but the gradient of kff wrt. l is somehow incorrect, needs another run

cspbo/RBF_mb.py
@@ -48,19 +48,19 @@ class RBF_mb():
         C_ee, C_ef, C_fe, C_ff = None, None, None, None
         C_ee_s, C_ef_s, C_fe_s, C_ff_s = None, None, None, None
         C_ee_l, C_ef_l, C_fe_l, C_ff_l = None, None, None, None
-            
         for key1 in data1.keys():
             for key2 in data2.keys():
                 if len(data1[key1])>0 and len(data2[key2])>0:
                     if key1 == 'energy' and key2 == 'energy':
                         C_ee, C_ee_s, C_ee_l = self.kee_many(data1[key1], data2[key2], True, True)
-
+                        #print(C_ee)
                     elif key1 == 'energy' and key2 == 'force':
                         C_ef, C_ef_s, C_ef_l = self.kef_many(data1[key1], data2[key2], True)
                         C_fe, C_fe_s, C_fe_l = C_ef.T, C_ef_s.T, C_ef_l.T
 
                     elif key1 == 'force' and key2 == 'force':
                         C_ff, C_ff_s, C_ff_l = self.kff_many(data1[key1], data2[key2], True, True)
+                        #print(C_ff)
         C = build_covariance(C_ee, C_ef, C_fe, C_ff)
         C_s = build_covariance(C_ee_s, C_ef_s, C_fe_s, C_ff_s)
         C_l = build_covariance(C_ee_l, C_ef_l, C_fe_l, C_ff_l)
@@ -105,38 +105,6 @@ class RBF_mb():
         else:
             return C
 
-    def kef_many(self, X1, X2, grad=False):
-        """
-        Compute the energy-force kernel between structures and atoms
-        Args:
-            X1: list of 2D arrays (each N*d)
-            X2: list of tuples ([X, dXdR])
-            grad: output gradient if true
-        Returns:
-            C: M*N 2D array
-            C_grad:
-        """
-        sigma, l = self.sigma, self.l
-        m1, m2 = len(X1), len(X2)
-        C = np.zeros([m1, 3*m2])
-        C_s = np.zeros([m1, 3*m2])
-        C_l = np.zeros([m1, 3*m2])
-
-        for i, x1 in enumerate(X1):
-            for j, data in enumerate(X2):
-                (x2, dx2dr) = data
-                if grad:
-                    Kef, Kef_sigma, Kef_l = kef_single_grad(x1, x2, dx2dr, sigma, l)
-                    C[i, j*3:(j+1)*3] = Kef
-                    C_s[i, j*3:(j+1)*3] = Kef_sigma
-                    C_l[i, j*3:(j+1)*3] = Kef_l
-                else:
-                    C[i, j*3:(j+1)*3] = kef_single(x1, x2, dx2dr, sigma**2, l**2)
-        if grad:
-            return C, C_s, C_l
-        else:
-            return C
-
     def kff_many(self, X1, X2, same=False, grad=False):
         """
         Compute the energy-force kernel between structures and atoms
@@ -149,7 +117,7 @@ class RBF_mb():
             C: M*N 2D array
             C_grad:
         """
-        sigma, l = self.sigma, self.l
+        sigma2, l2 = self.sigma**2, self.l**2
         m1, m2 = len(X1), len(X2)
         C = np.zeros([3*m1, 3*m2])
         C_s = np.zeros([3*m1, 3*m2])
@@ -163,21 +131,52 @@ class RBF_mb():
                 start = 0
             for j in range(start, len(X2)):
                 (x2, dx2dr) = X2[j]
-
                 if grad:
-                    Kff, dKff_sigma, dKff_l = kff_single_grad(x1, x2, dx1dr, dx2dr, sigma**2, l**2)
+                    Kff, dKff_sigma, dKff_l = kff_single_grad(x1, x2, dx1dr, dx2dr, sigma2, l2)
                     C[i*3:(i+1)*3, j*3:(j+1)*3] = Kff
                     C_s[i*3:(i+1)*3, j*3:(j+1)*3] = dKff_sigma
-                    C_s[j*3:(j+1)*3, i*3:(i+1)*3] = dKff_sigma
+                    C_s[j*3:(j+1)*3, i*3:(i+1)*3] = dKff_sigma.T
                     C_l[i*3:(i+1)*3, j*3:(j+1)*3] = dKff_l
-                    C_l[j*3:(j+1)*3, i*3:(i+1)*3] = dKff_l
+                    C_l[j*3:(j+1)*3, i*3:(i+1)*3] = dKff_l.T
                 else:
-                    C[i*3:(i+1)*3, j*3:(j+1)*3] = kff_single(x1, x2, dx1dr, dx2dr, sigma, l)
+                    C[i*3:(i+1)*3, j*3:(j+1)*3] = kff_single(x1, x2, dx1dr, dx2dr, sigma2, l2)
                 if same:
-                    C[j*3:(j+1)*3, i*3:(i+1)*3] = C[i*3:(i+1)*3, j*3:(j+1)*3]
-
+                    C[j*3:(j+1)*3, i*3:(i+1)*3] = C[i*3:(i+1)*3, j*3:(j+1)*3].T
         if grad:
             return C, C_s, C_l                  
+        else:
+            return C
+
+
+    def kef_many(self, X1, X2, grad=False):
+        """
+        Compute the energy-force kernel between structures and atoms
+        Args:
+            X1: list of 2D arrays (each N*d)
+            X2: list of tuples ([X, dXdR])
+            grad: output gradient if true
+        Returns:
+            C: M*N 2D array
+            C_grad:
+        """
+        sigma2, l2 = self.sigma**2, self.l**2
+        m1, m2 = len(X1), len(X2)
+        C = np.zeros([m1, 3*m2])
+        C_s = np.zeros([m1, 3*m2])
+        C_l = np.zeros([m1, 3*m2])
+
+        for i, x1 in enumerate(X1):
+            for j, data in enumerate(X2):
+                (x2, dx2dr) = data
+                if grad:
+                    Kef, Kef_sigma, Kef_l = kef_single_grad(x1, x2, dx2dr, sigma2, l2)
+                    C[i, j*3:(j+1)*3] = Kef
+                    C_s[i, j*3:(j+1)*3] = Kef_sigma
+                    C_l[i, j*3:(j+1)*3] = Kef_l
+                else:
+                    C[i, j*3:(j+1)*3] = kef_single(x1, x2, dx2dr, sigma2, l2)
+        if grad:
+            return C, C_s, C_l
         else:
             return C
 
@@ -220,30 +219,14 @@ def kef_single(x1, x2, dx2dr, sigma2, l2):
     x1_norm = np.linalg.norm(x1, axis=1)
     x2_norm = np.linalg.norm(x2, axis=1)
     _, d1 = fun_D(x1, x2, x1_norm, x2_norm)
-    dk_dx2 = fun_dk_dx2(x1, x2, x1_norm, x2_norm, d1, sigma2, l2)  
-    Kef = -np.einsum("ij, ijk->k", dk_dx2, dx2dr) # [m,d], [m,d,3] -> 3
-    return Kef
+    return K_ef(x1, x2, x1_norm, x2_norm, dx2dr, d1, sigma2, l2)
 
-def kef_single_grad(x1, x2, dx2dr, sigma, l):
+def kef_single_grad(x1, x2, dx2dr, sigma2, l2):
     """ Get the derivative of Kef with respect to sigma. """ 
     x1_norm = np.linalg.norm(x1, axis=1)
     x2_norm = np.linalg.norm(x2, axis=1)
     D1, d1 = fun_D(x1, x2, x1_norm, x2_norm) #D1=1-d1**2
-    k1 = np.exp(-0.5*D1/l**2)  # [N, M]
-    dk_dx2, dDdx2 = fun_dk_dx2(x1, x2, x1_norm, x2_norm, d1, sigma**2, l**2)  
-    Kef = -np.einsum("ij, ijk->k", dk_dx2, dx2dr) # [m,d], [m,d,3] -> 3
-
-
-    d2k_dDdsigma = fun_d2k_dDdsigma(x1, x2, x1_norm, x2_norm, sigma, l)
-    d2k_dDdl = fun_d2k_dDdl(x1, x2, x1_norm, x2_norm, sigma, l)
- 
-    tmp1 = np.einsum("ij,ijk->jk", d2k_dDdsigma, dD_dx2) # N,M  NMD 
-    dKef_dsigma = -np.einsum("ij,ijk->k", tmp1, dx2dr)
-
-    tmp2 = np.einsum("ij,ijk->jk", d2k_dDdl, dD_dx2) # mn, mnd2 -> nd2
-    dKef_dl = -np.einsum("ij,ijk->k", tmp2, dx2dr) #nd2, nd2 3
-    
-    return Kef, dKef_dsigma, dKef_dl
+    return K_ef(x1, x2, x1_norm, x2_norm, dx2dr, d1, sigma2, l2, True)
 
 
 def kff_single(x1, x2, dx1dr, dx2dr, sigma2, l2):
@@ -260,33 +243,14 @@ def kff_single(x1, x2, dx1dr, dx2dr, sigma2, l2):
     x1_norm = np.linalg.norm(x1, axis=1)
     x2_norm = np.linalg.norm(x2, axis=1)
     D1, d1 = fun_D(x1, x2, x1_norm, x2_norm)
-    d2k_dx1dx2, _ = fun_d2k_dx1dx2(x1, x2, x1_norm, x2_norm, d1, sigma2, l2)  # m, n, d1, d2
-    kff = np.einsum("ikm,ijkl->jlm", dx1dr, d2k_dx1dx2) #[m,d1,3], [m,n,d1,d2] -> n,d2,3
-    kff = np.einsum("ijk,ijl->kl", kff, dx2dr) # 3, 3
-    return kff
+    return K_ff(x1, x2, x1_norm, x2_norm, dx1dr, dx2dr, d1, sigma2, l2) 
     
-def kff_single_grad(x1, x2, dx1dr, dx2dr, sigma, l):
+def kff_single_grad(x1, x2, dx1dr, dx2dr, sigma2, l2):
     x1_norm = np.linalg.norm(x1, axis=1)
     x2_norm = np.linalg.norm(x2, axis=1)
     D1, d1 = fun_D(x1, x2, x1_norm, x2_norm)
-    tmp = np.exp(-0.5*D1/l**2)
-    d2k_dx1dx2, d2k_tmp = fun_d2k_dx1dx2(x1, x2, x1_norm, x2_norm, d1, sigma**2, l**2)  # m, n, d1, d2
-    kff = np.einsum("ikm,ijkl->jlm", dx1dr, d2k_dx1dx2)
-    kff = np.einsum("ijk,ijl->kl", kff, dx2dr)
+    return K_ff(x1, x2, x1_norm, x2_norm, dx1dr, dx2dr, d1, sigma2, l2, True) 
 
-    d2k_dDdsigma = fun_d2k_dDdsigma(x1, x2, x1_norm, x2_norm, sigma, l)
-    d2k_dDdl = fun_d2k_dDdl(x1, x2, x1_norm, x2_norm, sigma, l)
-    tmp1 = d2k_dDdsigma[:,:,None,None] * d2k_tmp
-    tmp2 = d2k_dDdl[:,:,None,None] * d2k_tmp
-
-    dKff_dsigma = np.einsum("ikm,ijkl->jlm", dx1dr, tmp1) #[N,3], [N,M] -> 3,M
-    dKff_dsigma = np.einsum("ijk,ijl->kl", dKff_dsigma, dx2dr) #[3,M], [M,3] -> 3,3
-
-    dKff_dl = np.einsum("ikm,ijkl->jlm", dx1dr, tmp2)
-    dKff_dl = np.einsum("ijk,ijl->kl", dKff_dl, dx2dr)
- 
-    return kff, dKff_dsigma, dKff_dl
- 
 def build_covariance(c_ee, c_ef, c_fe, c_ff):
     exist = []
     for x in (c_ee, c_ef, c_fe, c_ff):
@@ -297,7 +261,8 @@ def build_covariance(c_ee, c_ef, c_fe, c_ff):
     if False not in exist:
         return np.block([[c_ee, c_ef], [c_fe, c_ff]])
     elif exist == [False, False, True, True]: # F in train, E/F in predict
-        return np.vstack((c_fe, c_ff))
+        #print(c_fe.shape, c_ff.shape)
+        return np.hstack((c_fe, c_ff))
     elif exist == [True, True, False, False]: # E in train, E/F in predict
         return np.hstack((c_ee, c_ef))
     elif exist == [False, True, False, False]: # E in train, F in predict

cspbo/derivatives.py
@@ -19,14 +19,55 @@ def fun_dk_dD(x1, x2, x1_norm, x2_norm, sigma2, l2):
     _k, _ = fun_k(x1, x2, x1_norm, x2_norm, sigma2, l2)
     return -0.5*_k/l2
 
-def fun_d2k_dDdsigma(x1, x2, x1_norm, x2_norm, sigma, l):
-    _k, _ = fun_k(x1, x2, x1_norm, x2_norm, sigma**2, l**2)
-    return -_k/sigma/l**2
+def fun_d2k_dDdsigma(x1, x2, x1_norm, x2_norm, sigma2, l2):
+    _k, _ = fun_k(x1, x2, x1_norm, x2_norm, sigma2, l2)
+    return -_k/np.sqrt(sigma2)/l2
 
-def fun_d2k_dDdl(x1, x2, x1_norm, x2_norm, sigma, l):
+def fun_d2k_dDdl(x1, x2, x1_norm, x2_norm, sigma2, l2):
+    l3 = np.sqrt(l2)*l2
     D, _ = fun_D(x1, x2, x1_norm, x2_norm)
-    _k, _ = fun_k(x1, x2, x1_norm, x2_norm, sigma**2, l**2)
-    return _k*(-0.5*D/l**2 + 1)/l**3
+    k = sigma2*np.exp(-0.5*D/l2)
+    return k/l3*(-0.5*D/l2 + 1)
+
+def K_ff(x1, x2, x1_norm, x2_norm, dx1dr, dx2dr, d, sigma2, l2, grad=False):
+    dk_dD = fun_dk_dD(x1, x2, x1_norm, x2_norm, sigma2, l2) #m, n
+    d2D_dx1dx2 = fun_d2D_dx1dx2(x1, x2, x1_norm, x2_norm, d) #m, n, d1, d2
+    dD_dx1, _ = fun_dD_dx1(x1, x2, x1_norm, x2_norm, d)        #m, n, d1
+    dD_dx2, _ = fun_dD_dx2(x1, x2, x1_norm, x2_norm, d)        #m, n, d2
+    tmp = d2D_dx1dx2 - 0.5/l2*dD_dx1[:,:,:,None]*dD_dx2[:,:,None,:] # m, n, d1, d2
+    
+    K_ff_0 = np.einsum("ijkl,ikm->ijlm", tmp, dx1dr) # m, n, d2, 3
+    K_ff_0 = np.einsum("ijkl,jkm->ijlm", K_ff_0, dx2dr) # m, n, 3, 3
+    
+    Kff = np.einsum("ijkl,ij->kl", K_ff_0, dk_dD) # 3, 3
+
+    if grad:
+        d2k_dDdsigma = fun_d2k_dDdsigma(x1, x2, x1_norm, x2_norm, sigma2, l2) #m,n
+        d2k_dDdl = fun_d2k_dDdl(x1, x2, x1_norm, x2_norm, sigma2, l2) #m, n
+        dKff_dsigma = np.einsum("ijkl,ij->kl", K_ff_0, d2k_dDdsigma) 
+        dKff_dl     = np.einsum("ijkl,ij->kl", K_ff_0, d2k_dDdl)
+        return Kff, dKff_dsigma, dKff_dl
+    else:
+        return Kff
+
+def K_ef(x1, x2, x1_norm, x2_norm, dx2dr, d, sigma2, l2, grad=False):
+    dk_dD = fun_dk_dD(x1, x2, x1_norm, x2_norm, sigma2, l2) #m, n
+    dD_dx2, _ = fun_dD_dx2(x1, x2, x1_norm, x2_norm, d) #m, n, d2
+
+    K_ef_0 = -np.einsum("ijk,jkl->ijl", dD_dx2, dx2dr) # [m, n, d2] [n, d2, 3] -> [m,n,3]
+    Kef = np.einsum("ijk,ij->k", K_ef_0, dk_dD) # 3
+
+    if grad:
+        d2k_dDdsigma = fun_d2k_dDdsigma(x1, x2, x1_norm, x2_norm, sigma2, l2) #m,n
+        d2k_dDdl = fun_d2k_dDdl(x1, x2, x1_norm, x2_norm, sigma2, l2) #m, n
+        dKef_dsigma = np.einsum("ijk,ij->k", K_ef_0, d2k_dDdsigma) 
+        dKef_dl     = np.einsum("ijk,ij->k", K_ef_0, d2k_dDdl)
+        return Kef, dKef_dsigma, dKef_dl
+    else:
+        return Kef
+
+
+
 
 def fun_dd_dx1(x1, x2, x1_norm, x2_norm):  
     # x1: m,d
@@ -60,7 +101,6 @@ def fun_d2d_dx1dx2(x1, x2, x1_norm, x2_norm):
 
     tmp3 = np.eye(x2.shape[1])[None,:,:] - np.einsum('ij,ik->ijk',x2,x2)/x2_norm2[:,None,None] # n*d1*d2
     out2 = tmp3[None, :, :, :]/x1_norm[:,None, None,None]/x2_norm[None,:,None,None]
-    #out2 = tmp3[None, :, :]/x1_norm[:,None,None]/x2_norm[None,:,None] #[m,n,d2]
 
     return out2 - out1
 
@@ -97,19 +137,13 @@ def fun_dk_dx2(x1, x2, x1_norm, x2_norm, d, sigma2, l2):
     
 def fun_d2k_dx1dx2(x1, x2, x1_norm, x2_norm, d, sigma2, l2):
     dk_dD = fun_dk_dD(x1, x2, x1_norm, x2_norm, sigma2, l2) #m, n
-    
     d2D_dx1dx2 = fun_d2D_dx1dx2(x1, x2, x1_norm, x2_norm, d) #m, n, d, d
+    #d2D_dx1dx2 = np.transpose(d2D_dx1dx2, axes=(0,1,3,2))
     dD_dx1, _ = fun_dD_dx1(x1, x2, x1_norm, x2_norm, d)        #m, n, d1
     dD_dx2, _ = fun_dD_dx2(x1, x2, x1_norm, x2_norm, d)        #m, n, d2
-    tmp1 = np.einsum('ijk,ijl->ijkl', dD_dx1, dD_dx2)          #m, n, d1, d2
-    #_, dD_dx1 = fun_dD_dx1(x1, x2, x1_norm, x2_norm, d)        #m, d1
-    #_, dD_dx2 = fun_dD_dx2(x1, x2, x1_norm, x2_norm, d)        #m, d2
-    #tmp2 = np.einsum('ik,jl->ijkl', dD_dx1, dD_dx2)  #m, n, d1, d2
-
-    tmp = d2D_dx1dx2 - 0.5/l2*tmp1 #m, n, d1, d2
+    tmp = d2D_dx1dx2 - 0.5/l2*dD_dx1[:,:,:,None]*dD_dx2[:,:,None,:] # m, n, d1, d2
+    #tmp = np.transpose(tmp, axes=(0,1,3,2))
     return tmp*dk_dD[:,:,None,None], tmp
-
-
 
 if __name__ == "__main__":
    
@@ -126,15 +160,8 @@ if __name__ == "__main__":
         k = sigma2*(torch.exp(-0.5*D/l2).sum())
         return k 
  
-    def test_fun(target="d"):
-        m, n, k, sigma2, l2 = 2, 2, 3, 1.1, 0.9
-        x1 = np.random.random([m, k])
-        x2 = np.random.random([n, k])
-        x1_norm = np.linalg.norm(x1, axis=1)
-        x2_norm = np.linalg.norm(x2, axis=1)
-        D1, d1 = fun_D(x1, x2, x1_norm, x2_norm)
 
-
+    def test_fun(x1, x2, x1_norm, x2_norm, D1, d1, target="d"):
         if target == "d":
             fun_df_dx1 = fun_dd_dx1
             fun_df_dx2 = fun_dd_dx2
@@ -164,6 +191,7 @@ if __name__ == "__main__":
 
         print("testing ", target)
            
+
         print("df_dx1 from np")
         print(df_dx1_np)
         print("df_dx2 from np")
@@ -201,7 +229,41 @@ if __name__ == "__main__":
                 grad2 = torch.autograd.grad(d2, t_x1)
                 print(((grad2[0]-grad1[0])/eps).numpy())
 
-    test_fun("d")
-    test_fun("D")
-    test_fun("k")
+    m, n, k, sigma2, l2 = 2, 2, 3, 0.81, 2.1
+    x1 = np.random.random([m, k])
+    x2 = np.random.random([n, k])
+    x1_norm = np.linalg.norm(x1, axis=1)
+    x2_norm = np.linalg.norm(x2, axis=1)
+    D1, d1 = fun_D(x1, x2, x1_norm, x2_norm)
 
+    test_fun(x1, x2, x1_norm, x2_norm, D1, d1, "d")
+    test_fun(x1, x2, x1_norm, x2_norm, D1, d1, "d")
+    test_fun(x1, x2, x1_norm, x2_norm, D1, d1, "d")
+    
+    _x1 = torch.tensor(x1, requires_grad=True)
+    _x2 = torch.tensor(x2, requires_grad=True)
+
+    sigma = np.sqrt(sigma2)
+    l = np.sqrt(l2)
+
+    D = D_torch(_x1, _x2)
+    k = sigma**2*torch.exp(-0.5*D/l**2).sum()
+    print("dKdD")
+    print(torch.autograd.grad(k, D)[0])
+    print(fun_dk_dD(x1, x2, x1_norm, x2_norm, sigma2, l2))
+
+    def dkdD_torch(x1, x2, sigma, l):
+        k = k_torch(x1, x2, sigma**2, l**2)
+        return -0.5*k/l**2 
+ 
+    print("testing d2k_dDdsigma, d2kdDdl")
+    sigma = torch.tensor(sigma, requires_grad=True)
+    l = torch.tensor(l, requires_grad=True)
+    dkdD = dkdD_torch(_x1, _x2, sigma, l)
+
+    print("dkdD-torch:", dkdD)
+    print("dkdD-numpy:", np.sum(fun_dk_dD(x1, x2, x1_norm, x2_norm, sigma2, l2)))
+    print("d2k_dDdl-torch:", torch.autograd.grad(dkdD, l, retain_graph=True)[0])
+    print("d2k_dDdl-numpy:", np.sum(fun_d2k_dDdl(x1, x2, x1_norm, x2_norm, sigma2, l2)))
+    print("d2k_dDds-torch:", torch.autograd.grad(dkdD, sigma)[0])
+    print("d2k_dDds-numpy:", np.sum(fun_d2k_dDdsigma(x1, x2, x1_norm, x2_norm, sigma2, l2)))

cspbo/gaussianprocess_ef.py
@@ -25,7 +25,7 @@ class GaussianProcess():
                         strs += "{:6.3f} ".format(para)
                     from scipy.optimize import approx_fprime
                     print("from: ", grad)
-                    print("scipy", approx_fprime(params, self.log_marginal_likelihood, 1e-6))
+                    print("scipy", approx_fprime(params, self.log_marginal_likelihood, 1e-7))
                     #import sys
                     #sys.exit()
                     print(strs)
@@ -135,6 +135,12 @@ class GaussianProcess():
             kernel.update(params)
         if eval_gradient:
             K, K_gradient = kernel.k_total_with_grad(self.train_x)
+            #print(K[:3,:3])
+            #print(K[3:,3:])
+            #print(K[:3,3:])
+            #print(K[3:,:3])
+            #import sys
+            #sys.exit()
         else:
             K = kernel.k_total(self.train_x)
         K[np.diag_indices_from(K)] += self.noise

cspbo/utilities.py
@@ -269,21 +269,11 @@ def plot_two_body(model, des, kernel, figname):
     cell = 10*np.eye(3)
     dimers = [Atoms("2Si", positions=[[0,0,0], [r,0,0]], cell=cell) for r in rs]
     
-    d1s = []
-    d2s = []
+    xs = []
     for dimer in dimers:
-        if kernel.name == 'RBF_2b':
-            d1s.append(None)
-            d2s.append(get_2b(dimer))
-        elif kernel.name in ['RBF_mb', 'Dot_mb']:
-            d1s.append(des.calculate(dimer)['x'])
-            d2s.append(None)
-        else:
-            d1s.append(des.calculate(dimer)['x'])
-            d2s.append(get_2b(dimer))
-
-    ds = (d1s, d2s)
-    energies = model.predict(ds)
+        xs.append(des.calculate(dimer)['x'])
+    data = {"energy": xs}
+    energies = model.predict(data)
     plt.plot(rs, 2*energies, '-d', label='2-body')
     plt.legend()
     plt.xlabel('R (Angstrom)')

mics/Run_random_one.py
@@ -12,6 +12,7 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 import sklearn.gaussian_process as gp
 from scipy.spatial.distance import cdist
 from pyxtal.interface.gulp import GULP
+from pyxtal_ff.descriptors.SO3 import SO3
 from warnings import catch_warnings, simplefilter
 
 def parse_ref(struc, eng, ref, ref_eng):
@@ -22,8 +23,7 @@ def parse_ref(struc, eng, ref, ref_eng):
 
     return False
 
-def opt_acquisition(descriptors, model, sg, species, numIons, ff, trial=5):
-    
+def opt_acquisition(descriptors, model, sg, species, numIons, ff, trial=10):   
     # Here we only generate 10 structures, no optimization
     strucs = []
     energies = np.zeros(trial)
@@ -81,15 +81,21 @@ def surrogate(model, descriptors, std=True):
             return model.predict(descriptors, return_cov=True)
 
 
+from sklearn.preprocessing import StandardScaler  
+scaler = StandardScaler()  
 
 N = 2000
 n = 10
 sgs = range(1,231)
 species = ["Si"]
 numIons = [8]
-Rmax = 10
+Rmax = 10.0
 ff = "edip_si.lib" #"tersoff.lib"
-kernel = gp.kernels.Matern()
+#kernel = gp.kernels.Matern()
+#kernel = 1.0*gp.kernels.DotProduct() #+ gp.kernels.WhiteKernel()
+kernel = 1.0*gp.kernels.RBF() #+ gp.kernels.WhiteKernel()
+nmax, lmax, rcut = 4, 4, 5.0
+des = SO3(nmax=nmax, lmax=lmax, rcut=rcut, derivative=False)
 
 #compute the reference ground state
 from ase.lattice import bulk
@@ -134,9 +140,10 @@ ground_bo = 0
 print(f"\nRunning BO\n")
 energies = []
 descriptors = []
+
 for i in range(n):
     struc = PyXtal(sgs, species, numIons)
-    struc, eng, runtime, spg, error = process(struc, "GULP", ff)
+    struc, eng, runtime, spg, error = process(struc, "GULP", ff, filename='BO.db')
     if not error:
         eng = eng/len(struc)
         strs = "{:4d} {:8.4f} {:8.2f} seconds {:12s} ".format(i, eng, runtime, spg)
@@ -150,14 +157,18 @@ for i in range(n):
             
         pmg_struc = AseAtomsAdaptor().get_structure(struc)
         _des = RDF(pmg_struc, R_max=Rmax).RDF[1]
+        #_des = np.mean(des.calculate(struc)['x'], axis=0) 
         descriptors.append(_des)
         energies.append(eng)
         engs_bo.append(eng)
 
 # Define the surrogate model
 descriptors = np.array(descriptors)
-model = GaussianProcessRegressor(kernel)
+#scaler.fit(descriptors)
+#descriptors = scaler.transform(descriptors) 
+model = GaussianProcessRegressor(kernel, n_restarts_optimizer=4)
 model.fit(descriptors, energies)
+print(model.kernel_)
 
 for i in range(n, N):
     _struc, _ = opt_acquisition(descriptors, model, sgs, species, numIons, ff)
@@ -166,9 +177,11 @@ for i in range(n, N):
         eng = eng/len(struc)
         engs_bo.append(eng)
         pmg_struc = AseAtomsAdaptor().get_structure(struc)
-        opt_des = RDF(pmg_struc, R_max=Rmax).RDF[1]
+        opt_des = [RDF(pmg_struc, R_max=Rmax).RDF[1]]
+        #opt_des = np.mean(des.calculate(struc)['x'], axis=0) 
+        #opt_des = scaler.transform([opt_des]) 
         res = parse_ref(struc, eng, ref, ref_eng)
-        est, _ = surrogate(model, [opt_des])
+        est, _ = surrogate(model, opt_des)
         strs = "{:4d}/{:4d} {:8.4f} -> {:8.4f} {:8.2f} seconds {:12s} ".format(i, len(energies), est[0], eng, runtime, spg)
         if res:
             print(strs+'+++++++')
@@ -180,8 +193,12 @@ for i in range(n, N):
         if abs(est[0]-eng) > 0.1: 
             descriptors = np.vstack((descriptors, opt_des))
             energies.append(eng)
+            #scaler.fit(descriptors)
+            #descriptors = scaler.transform(descriptors) 
+            #model.kernel = model.kernel_
             model.fit(descriptors, energies)
-            print(strs + "update GP model")
+            print(strs + str(model.kernel_))
+            struc, eng, runtime, spg, error = process(struc, "GULP", ff, filename='BO.db')
         else:
             print(strs)
 t1 = time()
@@ -190,8 +207,8 @@ print("Complete in {:.3f}s".format(t1-t0))
 #print(engs_bo)
 #print(engs_random)
 bins = np.linspace(ref_eng - 0.1, ref_eng + 3.0, 150)
-label1 = "Random: {:d}/{:d}".format(ground_rand, len(engs_random))
-label2 = "BO: {:d}/{:d}".format(ground_bo, len(engs_bo))
+label1 = "Random: {:d}/{:d}".format(ground_rand, N)
+label2 = "BO: {:d}/{:d}".format(ground_bo, N)
 plt.hist(engs_random, bins, alpha=0.5, label=label1)
 plt.hist(engs_bo, bins, alpha=0.5, label=label2)
 plt.xlabel("Energy (eV/atom)")

mics/bo_run.py
@@ -39,10 +39,17 @@ def acquisition(descriptors, _descriptors, model, style="PI"):
         ix = np.argmax(probs)
 
     elif style == "Thompson":
-        alpha = 1
+        alpha = 0 #1
         mu, cov = surrogate(model, _descriptors, std=False)
         mu_1d = mu.ravel()
+        print(mu_1d)
         score = np.random.multivariate_normal(mu_1d, cov * alpha ** 2, 1)
+        print(score)
+        alpha = 1
+        score = np.random.multivariate_normal(mu_1d, cov * alpha ** 2, 1)
+        print(score)
+        import sys
+        sys.exit()
         score = score[0, :]
         ix = np.argmax(score)   
         
@@ -61,8 +68,8 @@ N = 100 # number of BO runs
 Rmax = 10
 sgs = range(1,231)
 species = ["Si"]
-numIons = [4]
-ff = "edip_si.lib" #"tersoff.lib"
+numIons = [8]
+ff = "edip_si.lib" 
 
 #compute the reference ground state
 from ase.lattice import bulk
@@ -82,9 +89,11 @@ for p in range(N):
     energies = []
     for i in range(n):
         struc = PyXtal(sgs, species, numIons)
-        struc, eng, spg, _ = process(struc, "GULP", ff, p, "bo.db")
+        struc, eng, _, spg = process(struc, "GULP", ff, p, "bo.db")
         pmg_struc = AseAtomsAdaptor().get_structure(struc)
         _des = RDF(pmg_struc, R_max=Rmax).RDF[1]
+
+        print("{:4d} {:12s} {:8.5f}".format(i, spg, eng/len(struc)))
         if i == 0:
             columns = _des.shape[0]
             descriptors = np.zeros([n, columns])
@@ -104,19 +113,12 @@ for p in range(N):
         pmg_struc = AseAtomsAdaptor().get_structure(struc)
         opt_des = RDF(pmg_struc, R_max=Rmax).RDF[1]
         est, _ = surrogate(model, [opt_des])
-        print("{:4d} {:12s} {:8.5f} -> {:8.5f} {:8.5f} ".format(count, spg, opt_eng, est[0], opt_eng-est[0]))
+        print("{:4d} {:12s} {:8.5f} -> {:8.5f} {:8.5f} ".format(count, spg, est[0], opt_eng, opt_eng-est[0]))
 
-        #descriptors = np.vstack((descriptors, [optimal_descriptor]))
-        #energies.append([optimal_energy])
-        #print(descriptors.shape)
-        #print(len(energies))
         descriptors = np.vstack((descriptors, opt_des))
         energies.append(opt_eng)
         model.fit(descriptors, energies)
         count += 1
-        #if abs(eng/len(struc) + 1.46946) < 1e-2: 
-        #    #print(pmg_struc.to(fmt='poscar'))
-        #    print(opt_des)
 
         if abs(eng/len(struc) - ref_eng) < 1e-2: 
             s_pmg = AseAtomsAdaptor().get_structure(struc)
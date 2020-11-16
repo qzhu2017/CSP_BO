fit the derivative bug in RBF_mb

cspbo/RBF_mb.py
@@ -491,14 +491,16 @@ def K_ff(x1, x2, dx1dr, dx2dr, sigma2, l2, zeta=2, grad=False, mask=None, eps=1e
         Kff = (K_ff_0 * dk_dD[:,:,None,None]).sum(axis=0)
 
         d2k_dDdsigma = 2*dk_dD/np.sqrt(sigma2)
-        d2k_dDdl = (D/l3)*dk_dD + (2/l)*dk_dD
+        d2k_dDdl = ((D-1)/l3 + 2/l)*dk_dD
+        #d2k_dDdl = (D/l3 + 2/l)*dk_dD
 
         dKff_dsigma = (K_ff_0 * d2k_dDdsigma[:,:,None,None]).sum(axis=0)
-        dKff_dl = (K_ff_0 * d2k_dDdl[:,:,None,None]).sum(axis=0)
-
-        K_ff_1 = (dD_dx1_dD_dx2[:,:,:,:,None] * dx1dr[:,None,:,None,:]).sum(axis=2)
+        dKff_dl = (-K_ff_0 * d2k_dDdl[:,:,None,None]).sum(axis=0)
+        
+        tmp = -dD_dx1_dD_dx2/l*2
+        K_ff_1 = (tmp[:,:,:,:,None] * dx1dr[:,None,:,None,:]).sum(axis=2)
         K_ff_1 = (K_ff_1[:,:,:,:,None] * dx2dr[None,:,:,None,:]).sum(axis=2)
-        dKff_dl += (K_ff_1 * dk_dD[:,:,None,None]).sum(axis=0)/(l2*np.sqrt(l2))
+        dKff_dl += (K_ff_1 * dk_dD[:,:,None,None]).sum(axis=0) #/l*2
         if device == 'cpu':
             return np.concatenate((Kff, dKff_dsigma, dKff_dl), axis=-1)
         else:
@@ -556,10 +558,11 @@ def K_ef(x1, x2, dx2dr, sigma2, l2, zeta=2, grad=False, mask=None, eps=1e-8, dev
         l = np.sqrt(l2)
         l3 = l2*l
         d2k_dDdsigma = 2*dk_dD/np.sqrt(sigma2)
-        d2k_dDdl = (D/l3)*dk_dD + (2/l)*dk_dD
+        d2k_dDdl = -((D-1)/l3 + 2/l)*dk_dD
 
         dKef_dsigma = (kef1*d2k_dDdsigma[:,:,None]).sum(axis=0) 
         dKef_dl     = (kef1*d2k_dDdl[:,:,None]).sum(axis=0)
+        #print(dKef_dl/m)#; import sys; sys.exit()
         if device == 'gpu':
             return cp.concatenate((Kef/m, dKef_dsigma/m, dKef_dl/m), axis=-1)
         else:

cspbo/gaussianprocess_ef.py
@@ -45,8 +45,14 @@ class GaussianProcess():
         s = "------Gaussian Process Regression------\n"
         s += "Kernel: {:s}".format(str(self.kernel))
         if hasattr(self, "train_x"):
-            s += " {:d} energy ({:.3f})".format(len(self.train_x["energy"][-1]), self.noise_e)
-            s += " {:d} forces ({:.3f})\n".format(len(self.train_x["force"][-1]), self.noise_f)
+            N_energy, N_force = 0, 0
+            if len(self.train_x["energy"]) > 0:
+                N_energy = len(self.train_x["energy"][-1])
+            if len(self.train_x["force"]) > 0:
+                N_force = len(self.train_x["force"][-1])
+
+            s += " {:d} energy ({:.3f})".format(N_energy, self.noise_e)
+            s += " {:d} forces ({:.3f})\n".format(N_force, self.noise_f)
         return s
 
     def __repr__(self):
@@ -72,7 +78,10 @@ class GaussianProcess():
                         strs += "{:6.3f} ".format(para)
                     #from scipy.optimize import approx_fprime
                     #print("from ", grad)
+                    #print("scipy", approx_fprime(params, self.log_marginal_likelihood, 1e-3))
+                    #print("scipy", approx_fprime(params, self.log_marginal_likelihood, 1e-4))
                     #print("scipy", approx_fprime(params, self.log_marginal_likelihood, 1e-5))
+                    #print("scipy", approx_fprime(params, self.log_marginal_likelihood, 1e-6))
                     print(strs)
                     #import sys; sys.exit()
                 return (-lml, -grad)
@@ -201,7 +210,8 @@ class GaussianProcess():
 
         for key in data.keys():
             if key == 'energy':
-                self.add_train_pts_energy(data[key])
+                if len(data[key])>0:
+                    self.add_train_pts_energy(data[key])
             elif key == 'force':
                 if len(data[key])>0:
                     self.add_train_pts_force(data[key])
@@ -423,7 +433,10 @@ class GaussianProcess():
         # add noise matrix
         #K[np.diag_indices_from(K)] += self.noise
         noise = np.eye(len(K))
-        NE = len(self.train_x['energy'][-1])
+        if len(self.train_x['energy']) > 0:
+            NE = len(self.train_x['energy'][-1])
+        else:
+            NE = 0
         noise[:NE,:NE] *= params[-1]**2
         noise[NE:,NE:] *= (self.f_coef*params[-1])**2
         K += noise
@@ -652,6 +665,9 @@ class GaussianProcess():
             energy_in = False
         #print(E_std, tol_e_var, energy_in)
         #import sys; sys.exit()
+        #N_energy = 0
+        #energy_in = False
+
         force_in = []
 
         xs_added = []
@@ -676,7 +692,7 @@ class GaussianProcess():
         N_pts = N_energy + N_forces
         if N_pts > 0:
             pts_to_add["db"].append((atoms, energy, force, energy_in, force_in))
-            print("{:d} energy and {:d} forces will be added".format(N_energy, N_forces))
+            #print("{:d} energy and {:d} forces will be added".format(N_energy, N_forces))
         errors = (E[0]+energy_off, E1[0]+energy_off, E_std, F+force_off.flatten(), F1+force_off.flatten(), F_std) 
         return pts_to_add, N_pts, errors
 

example_sampling.py
@@ -1,30 +1,32 @@
 import sys
 import numpy as np
 from time import time
-from cspbo.utilities import metric_single, build_desc, get_data, get_train_data, rmse, plot, plot_two_body
+from cspbo.utilities import metric_single, build_desc, get_data, get_train_data, rmse
 from cspbo.gaussianprocess_ef import GaussianProcess as gpr
 from cspbo.calculator import GPR, LJ
 from cspbo.RBF_mb import RBF_mb
 from cspbo.Dot_mb import Dot_mb
 
-#N_start, N_step, N_max, zeta, ncpu, fac = 4, 1, 2554, 2, 10, 1.2
 N_start, N_step, N_max, zeta, device, fac, N_force = 1, 1, None, 2, 'gpu', 1.2, 8
+#N_start, N_step, N_max, zeta, device, fac, N_force = 1, 1, None, 2, 'cpu', 1.2, 8
 
 if len(sys.argv) == 2:
     des = build_desc("SO3", lmax=3, nmax=3, rcut=4.0)
     kernel = RBF_mb(para=[1, 0.5], zeta=zeta, device=device)
-    kernel = Dot_mb(para=[2, 0.5], zeta=zeta, device=device)
-    lj = None #LJ(parameters={"rc": 5.0, "sigma": 2.13})
+    #kernel = Dot_mb(para=[2, 0.5], zeta=zeta, device=device)
+    lj = None #LJ(parameters={"rc": 6.0, "sigma": 2.13})
     
     model = gpr(kernel=kernel, 
                 descriptor=des, 
                 base_potential=lj,
-                noise_e=[5e-3, 2e-3, 5e-3], 
-                f_coef=30,
+                noise_e=[5e-3, 5e-3, 3e-2], 
+                f_coef=20,
                )
     db_file = sys.argv[1]
     db_ids = range(N_start)
+    #train_data = get_data(db_file, des, N_force=5, lists=db_ids, select=True, no_energy=True)
     train_data = get_data(db_file, des, N_force=5, lists=db_ids, select=True)
+    #train_data = get_data(db_file, des, N_force=10, lists=db_ids, select=True)
     model.fit(train_data)
 else: #pick a model from the previous calcs
     m_file = sys.argv[1]
@@ -44,21 +46,24 @@ print("\n")
 if N_max is None:
     N_max = len(strucs)
 
-for id in range(1200, N_max):
-#for id in range(N_max):
+#for id in range(1200, N_max):
+for id in range(N_max):
     data = (strucs[id], energies[id], forces[id])
+    N_atom = len(strucs[id])
     pts, N_pts, (E, E1, E_std, F, F1, F_std) = model.add_structure(data)
     print("{:4d} E_True/Pred: {:8.4f} -> {:8.4f} Error_E/F: {:8.4f}[{:8.4f}] {:8.4f}[{:8.4f}]".format(\
         id, E, E1, E-E1, E_std, rmse(F, F1), np.max(F_std)))
-    
+    #if id == 406: import sys; sys.exit()
     if N_pts > 0:
         model.set_train_pts(pts, mode="a+")
-        model.fit()
+        model.fit(opt=False, show=False)
+    if id % 10 == 0:
+        model.sparsify(e_tol=1e-4, f_tol=1e-3)
+        #model.fit()
         train_E, train_E1, train_F, train_F1 = model.validate_data()
         l1 = metric_single(train_E, train_E1, "Train Energy") 
         l2 = metric_single(train_F, train_F1, "Train Forces") 
         print(model)
-    if id % 100 == 0:
-        model.save("models/test.json", "models/test.db")
+    if (id+1) % 100 == 0:
+        model.save("models/sample.json", "models/sample.db")
 model.save("models/test.json", "models/test.db")
-plot_two_body(model, "2-body.png")

example_validate.py
@@ -16,8 +16,8 @@ N_max, device = 10, 'gpu'
 m_file = sys.argv[1]
 db_file = sys.argv[2]
 model = gpr()
-#model.load(m_file, N_max=2, opt=True, device=device)
-model.load(m_file, N_max=None, opt=True, device=device)
+model.load(m_file, N_max=100, opt=True, device=device)
+#model.load(m_file, N_max=None, opt=True, device=device)
 print(model.kernel.device)
 
 train_E, train_E1, train_F, train_F1 = model.validate_data()
@@ -28,13 +28,13 @@ l2 = metric_single(train_F, train_F1, "Train Forces")
 strucs, values = get_strucs(db_file, N_max=N_max)
 (E0, F0, S0) = values[0]
 stress = False
-if isinstance(S0, np.ndarray):
-    try:
-        if S0[0] is not None:
-            stress = True
-    except:
-        if S0 !=  None:
-            stress = True
+#if isinstance(S0, np.ndarray):
+#    try:
+#        if S0[0] is not None:
+#            stress = True
+#    except:
+#        if S0 !=  None:
+#            stress = True
 
 print(stress)
 # set calculator